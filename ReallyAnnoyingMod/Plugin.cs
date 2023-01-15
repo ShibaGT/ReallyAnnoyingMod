2212137287792170c32db8f7762d6993aed680fa
shibagt <shibagtsalt@gmail.com>
2023-01-15 14:36:53 -0700

    initial commit

---
+++ ReallyAnnoyingMod/Plugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using System.ComponentModel;
+using Utilla;
+using Photon.Pun;
+
+namespace ReallyAnnoyingMod
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [Description("HauntedModMenu")]
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        bool on;
+
+        void Start()
+        {
+            on = false;
+            inRoom = false;
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            on = true;
+
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        void OnDisable()
+        {
+            on = false;
+
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+
+        }
+
+        void Update()
+        {
+            if (PhotonNetwork.InRoom)
+            {
+                if (on)
+                {
+                    System.Random rnd = new System.Random();
+                    GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(rnd.Next(3, 153), false, 0.5f);
+                }
+            }
+        }
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+        }
+    }
+}
---
+++ ReallyAnnoyingMod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ReallyAnnoyingMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.shibagt.gorillatag.reallyannoyingmod";
+        public const string Name = "ReallyAnnoyingMod";
+        public const string Version = "1.0.0";
+    }
+}
